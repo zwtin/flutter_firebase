21e0e66a6a4d6df7703b4de94366b92ed8728176
zwtin <masa100928@gmail.com>
2020-02-16 21:55:59 +0900

    cupertinotabbarを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,99 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_firebase/blocs/authentication/authentication_bloc.dart';
-import 'package:flutter_firebase/blocs/authentication/authentication_event.dart';
-import 'package:flutter_firebase/blocs/authentication/authentication_state.dart';
-import 'package:flutter_firebase/repositories/firebase_authentication_repository.dart';
-import 'package:flutter_firebase/screens/event_list_screen.dart';
-import 'package:flutter_firebase/screens/sign_in_screen.dart';
-import 'package:flutter_firebase/screens/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
-  final authenticationRepository = FirebaseAuthenticationRepository();
   Crashlytics.instance.enableInDevMode = true;
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runZoned(() {
-    runApp(
-      BlocProvider<AuthenticationBloc>(
-        create: (context) =>
-            AuthenticationBloc(authRepository: authenticationRepository)
-              ..add(AppStarted()),
-        child: MyApp(),
-      ),
-    );
+    runApp(MyApp());
   }, onError: Crashlytics.instance.recordError);
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  int currentIndex = 0;
+
+  final List<Tab> tabs = <Tab>[
+    Tab(
+      icon: Icon(Icons.home),
+    ),
+    Tab(
+      icon: Icon(Icons.person),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final authenticationBloc = BlocProvider.of<AuthenticationBloc>(context);
-    return MaterialApp(
-      title: 'Awase',
-      theme: ThemeData(
-          primaryColor: Colors.indigo[900],
-          accentColor: Colors.pink[800],
-          brightness: Brightness.light),
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-          bloc: authenticationBloc,
-          builder: (context, state) {
-            if (state is AuthenticationInProgress) {
-              return SplashScreen();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              currentIndex = 0;
+              break;
+            case 1:
+              currentIndex = 1;
+              break;
+          }
+        },
+        activeColor: Colors.blue,
+        inactiveColor: Colors.black,
+        backgroundColor: Colors.red,
+      ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(
+          builder: (context) {
+            switch (currentIndex) {
+              case 0:
+                return Center(
+                  child: Text(
+                    'text',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                );
+                break;
+              case 1:
+                return Scaffold();
+                break;
             }
-            if (state is AuthenticationSuccess) {
-              return EventListScreen(authenticationBloc: authenticationBloc);
-            }
-            if (state is AuthenticationFailure) {
-              return SignInScreen();
-            }
-            return Container();
-          }),
+            return Scaffold();
+          },
+        );
+      },
     );
   }
 }
