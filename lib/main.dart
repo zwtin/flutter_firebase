32d4632b8803844f50fcc9c03da0c63b82603f0a
zwtin <masa100928@gmail.com>
2020-02-17 02:23:50 +0900

    tabScreenを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_firebase/screens/tab_screen.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter_firebase/blocs/authentication/authentication_bloc.dart';
-import 'package:flutter_firebase/blocs/authentication/authentication_repository.dart';
-import 'package:flutter_firebase/repositories/firebase_authentication_repository.dart';
-
-import 'package:flutter_firebase/screens/sign_in_screen.dart';
-import 'package:flutter_firebase/screens/event_list_screen.dart';
-
-import 'package:flutter_firebase/CalcSample/calc_provider.dart';
-import 'package:flutter_firebase/CalcSample/screen.dart';
 
 void main() {
   Crashlytics.instance.enableInDevMode = true;
@@ -21,7 +13,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,68 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  final CupertinoTabController _cupertinoTabController =
-      CupertinoTabController();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.person),
-          ),
-        ],
-        activeColor: Colors.blue,
-        inactiveColor: Colors.black,
-        backgroundColor: Colors.red,
-      ),
-      controller: _cupertinoTabController,
-      tabBuilder: (BuildContext context, int index) {
-        return CupertinoTabView(
-          builder: (context) {
-            switch (index) {
-              case 0:
-                return Center(
-                  child: CupertinoButton(
-                    child: const Text('Go to first tab'),
-                    onPressed: () {
-                      _cupertinoTabController.index = 1;
-                    },
-                  ),
-                );
-                break;
-              case 1:
-                return MaterialApp(
-                  home: CalcBlocProvider(
-                    child: CalcScreen(),
-                  ),
-                );
-                break;
-            }
-            return Scaffold();
-          },
-        );
-      },
+      home: TabScreen(),
     );
   }
 }
---
+++ lib/screens/tab_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_firebase/CalcSample/calc_provider.dart';
+import 'package:flutter_firebase/CalcSample/screen.dart';
+
+class TabScreen extends StatelessWidget {
+  final CupertinoTabController _cupertinoTabController =
+      CupertinoTabController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+          ),
+        ],
+        activeColor: Colors.blue,
+        inactiveColor: Colors.black,
+        backgroundColor: Colors.red,
+      ),
+      controller: _cupertinoTabController,
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (context) {
+            switch (index) {
+              case 0:
+                return Center(
+                  child: CupertinoButton(
+                    child: const Text('Go to first tab'),
+                    onPressed: () {
+                      _cupertinoTabController.index = 1;
+                    },
+                  ),
+                );
+                break;
+              case 1:
+                return MaterialApp(
+                  home: CalcBlocProvider(
+                    child: CalcScreen(),
+                  ),
+                );
+                break;
+            }
+            return Scaffold();
+          },
+        );
+      },
+    );
+  }
+}
