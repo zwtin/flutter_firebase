8826a5a63fd1e0911a6bacf643ebb9c84920a360
zwtin <masa100928@gmail.com>
2020-02-24 04:45:03 +0900

    eventListにListViewを実装

--- lib/screens/event_list_screen.dart
+++ lib/screens/event_list_screen.dart
@@ -51,7 +51,112 @@ class EventListScreen extends StatelessWidget {
               child: CircularProgressIndicator(),
             ),
           );
+        } else if (snapshot.hasData && snapshot.data is EventListEmpty) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              title: Text(
+                'AppBar',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: Colors.orange,
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.face,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            body: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: InkWell(
+                    onTap: () => eventListBloc.read.add(null),
+                    child: Padding(
+                      child: Text(
+                        '$index',
+                        style: TextStyle(fontSize: 22.0),
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
         } else if (snapshot.hasData && snapshot.data is EventListFailure) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              title: Text(
+                'AppBar',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: Colors.orange,
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.face,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('no user login'),
+                  RaisedButton(
+                    child: const Text('Button'),
+                    color: Colors.orange,
+                    textColor: Colors.white,
+                    onPressed: () => eventListBloc.read.add(null),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else if (snapshot.hasData && snapshot.data is EventListSuccess) {
           return Scaffold(
             appBar: AppBar(
               leading: Icon(
